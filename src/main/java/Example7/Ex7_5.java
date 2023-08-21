712cecf233c2d32af73b5235e8087ec567180e2b
leeminho <tommymh97@naver.com>
2023-08-22 04:16:12 +0900

    7chapter

---
+++ src/main/java/Example7/Ex7_5.java
@@ -0,0 +1,7 @@
+package Example7;
+
+class Ex7_5 {
+    public static void main(String[] args){
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/main/java/Example7/Ex7_6.java
@@ -0,0 +1,12 @@
+package Example7;
+
+import static java.lang.System.out;
+import static java.lang.Math.*;
+
+class Ex7_6 {
+    public static void main(String[] args){
+        out.println(random());
+
+        out.println("Math.PI :" + PI);
+    }
+}
