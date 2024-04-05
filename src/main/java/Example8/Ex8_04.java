5d953a7497ecb75372796096de0b43c542ba2d7c
leeminho <tommymh97@naver.com>
2024-04-05 19:55:34 +0900

    8chapter

--- src/main/java/Example8/Ex8_04.java
+++ src/main/java/Example8/Ex8_04.java
@@ -12,9 +12,13 @@ class Ex8_04 {
             if ( ae instanceof ArithmeticException)
                 System.out.println("true");
             System.out.println("ArithmeticException");
-        } catch (Exception e){
+        } catch (Exception e){                              // ArithmeticException을 제외한 모든 예외 처리
             System.out.println("Exception");
         }   //try-catch 의 끝
         System.out.println(6);
     }   //main 메서드의 끝
 }
+
+// try 블럭에서 ArithmeticException이 발생하였으므로 차례로 catch 블럭 검사
+
+// try-catch문의 마지막에 Exception 타입의 참조변수를 선언한 catch블럭을 사용하면, 어떤 종류의 예외가 발생하더라도 이 catch 블럭에 의해 처리 가능
